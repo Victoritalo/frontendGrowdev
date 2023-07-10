0f8cc3eb2e2e3b7750290ff56c175012596db491
Victor Italo <victoritalo@live.com>
2023-07-10 00:50:42 -0300

    :books: Docs: Update aula

--- introJavaScript/atividadeLogicaJS/atividadePratica_createApiNodeJS/server.js
+++ introJavaScript/atividadeLogicaJS/atividadePratica_createApiNodeJS/server.js
@@ -1,21 +1,73 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const Product = require("./models/productModel");
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
+//Save a product
+app.post("/products", async (req, res) => {
+  try {
+    const product = await Product.create(req.body);
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
-app.get("/cadastro", (req, res) => {
-  res.send("Hello, my name is Vic");
+//Fetch all products
+app.get("/products", async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
-app.use(express.json())
-app.post("/product", (req, res) => {
-  res.send(req.body);
+//Fetch product by ID
+app.get("/products/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+//Update product by ID
+app.put("/products/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findByIdAndUpdate(id, req.body);
+    if (!product) {
+      return res
+        .status(404)
+        .json({ message: `Cannot find any product with ID: ${id}` });
+    }
+    const updatedProduct = await Product.findById(id);
+    res.status(200).json(updatedProduct);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+//Delete
+app.delete("/products/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findByIdAndDelete(id);
+    if (!product) {
+      return res
+        .status(404)
+        .json({ message: `Cannot find any product with ID: ${id}` });
+    }
+    res.status(200).json(product)
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
 
 mongoose
   .connect(
-    
+
   )
   .then(() => {
     console.log("Connected!");
