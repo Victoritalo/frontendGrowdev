432c581f8d4da883325d1eb1ec4e176aca4e091e
Victor Italo <victoritalo@live.com>
2023-07-15 23:26:58 -0300

    :books: Add register route

--- introJavaScript/atividadeLogicaJS/atividadePraticaJS-CrudNodeJS/index.js
+++ introJavaScript/atividadeLogicaJS/atividadePraticaJS-CrudNodeJS/index.js
@@ -1,8 +1,34 @@
 import express from "express";
 const app = express();
+app.use(express.json());
+const usersData = [];
+let userId = 0;
 
-app.get("/cadastro", function (req, res) {
-  res.send("Hello World!");
+
+app.post("/user/signup", (req, res) => {
+  const emailExists = usersData.some(email => {
+    return email === req.body.userEmail
+  })
+  if(emailExists){
+    res.status(400)
+    res.send("Email already registered by another user!")
+  } else{
+    const newUser = {
+      "userEmail": req.body.userEmail,
+      "userName": req.body.userName,
+      "userPass": req.body.userPass,
+      "id": userId,
+      "userMsgs": [],
+    };
+    userId++;
+    usersData.push(newUser);
+    res.status(200).json(usersData)
+  }
+
+});
+
+app.get("/user/:id", (req, res) => {
+  res.send(usersData.id);
 });
 
 app.listen(3000, () => {
