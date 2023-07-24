52c9003e9ee93d2d7e873016dd02733a57ded762
Victor Italo <victoritalo@live.com>
2023-07-23 22:55:10 -0300

    update Aula

--- introJavaScript/atividadeLogicaJS/atividadePraticaJS-CrudNodeJS/index.js
+++ introJavaScript/atividadeLogicaJS/atividadePraticaJS-CrudNodeJS/index.js
@@ -2,25 +2,11 @@ import express from "express";
 const app = express();
 app.use(express.json());
 
-const usersData = [
-  // {
-  //   userEmail: "reginaldo@victor.com",
-  //   userName: "Reginaldo",
-  //   userPass: 4444,
-  //   userId: 0,
-  //   userMsgs: [{ messageId: 0, title: "Hello", message: "Hello World!" }],
-  // },
-  // {
-  //   userEmail: "victor@victor.com",
-  //   userName: "Victor",
-  //   userPass: 4444,
-  //   userId: 1,
-  //   userMsgs: [{ message: "Hello Reginaldo" }, { message: "Hello Reginaldo" }],
-  // },
-];
+const usersData = [];
 let userUniqueId = 0;
 let userMsgId = 2345234;
-app.post("/user/signup", (req, res) => {
+
+app.post("/signup", (req, res) => {
   const emailExists = usersData.some((user) => {
     return req.body.userEmail === user.userEmail;
   });
@@ -28,29 +14,26 @@ app.post("/user/signup", (req, res) => {
   if (emailExists) {
     res.status(400);
     res.send("Email already registered by another user!");
+    return;
   } else {
     const newUser = {
       userEmail: req.body.userEmail,
       userName: req.body.userName,
       userPass: req.body.userPass,
       userId: userUniqueId,
-      userMsgs: [
-        { messageId: userMsgId, title: "Welcome", message: "Hello World!" },
-      ],
+      userMsgs: [],
     };
     userUniqueId++;
-    userMsgId++;
     usersData.push(newUser);
     res.status(200).json(usersData);
   }
 });
 
-//temporary /users only for tests
 app.get("/users", (req, res) => {
   res.status(200).json({ UsersList: usersData });
 });
 
-app.get("/user/login", (req, res) => {
+app.get("/login", (req, res) => {
   const verifyUser = usersData.find((user) => {
     return (
       user.userEmail === req.body.userEmail &&
@@ -58,37 +41,104 @@ app.get("/user/login", (req, res) => {
     );
   });
   if (verifyUser) {
-    res.status(200).json({ message: "Login successful!", verifyUser });
+    res.status(200).json({
+      message: "Login successful!",
+      name: verifyUser.userName,
+      userID: verifyUser.userId,
+      Messages: verifyUser.userMsgs,
+    });
   } else {
     res.status(400).json({ message: "Wrong credentials!" });
+    return;
   }
 });
 
-app.post("/user/:userId", (req, res) => {
+//Create Message
+app.post("/:userId", (req, res) => {
   const userId = parseInt(req.params.userId);
-  const userMessage = usersData.find((user) => {
+  const findUser = usersData.find((user) => {
     return user.userId === userId;
   });
 
-  if (userMessage) {
+  if (!findUser) {
+    res.status(404).json({ message: "User not found!" });
+    return;
+  } else {
+    const newMessage = {
+      messageId: userMsgId,
+      title: req.body.title,
+      message: req.body.message,
+    };
+    findUser.userMsgs.push(newMessage);
+    userMsgId++;
     res
       .status(200)
-      .json({ user: userMessage.userName, message: userMessage.userMsgs });
-  } else {
-    res.status(404).json({ message: "User not found!" });
+      .json({ user: findUser.userName, message: findUser.userMsgs });
   }
+});
 
-  const newMessage = {
-    messageId: userMsgId,
-    title: req.body.title,
-    message: req.body.message,
-  };
-  usersData.userMsgs.push(newMessage);
-  userMsgId++;
-  res.status(200);
-  res.send(newMessage);
-  console.log(newTitle);
-  console.log(newMsg);
+//Update
+app.put("/:userId/:messageId", (req, res) => {
+  const userId = parseInt(req.params.userId);
+  const messageId = parseInt(req.params.messageId);
+
+  const findUser = usersData.find((user) => {
+    return user.userId === userId;
+  });
+
+  if (!findUser) {
+    res.status(404).json({
+      message:
+        "User does not exist or has no permission to access this message",
+    });
+    return;
+  } else {
+    const findMessage = findUser.userMsgs.find((message) => {
+      return message.messageId === messageId;
+    });
+
+    if (!findMessage) {
+      res.status(404).json({ message: "Message does not exist" });
+      return;
+    } else {
+      const updateTitle = req.body.title;
+      const updateMsg = req.body.message;
+      findMessage.title = updateTitle;
+      findMessage.message = updateMsg;
+    }
+    res
+      .status(200)
+      .json({ user: findUser.userName, message: findUser.userMsgs });
+  }
+});
+
+app.delete("/:userId/:messageId", (req, res) => {
+  const userId = parseInt(req.params.userId);
+  const messageId = parseInt(req.params.messageId);
+
+  const findUser = usersData.find((user) => {
+    return user.userId === userId;
+  });
+
+  if (!findUser) {
+    res.status(404).json({
+      message:
+        "User does not exist or has no permission to access this message",
+    });
+  } else {
+    const findMessage = findUser.userMsgs.find((message) => {
+      return message.messageId === messageId;
+    });
+
+    if (!findMessage) {
+      res.status(404).json({ error: "Message does not exist" });
+    } else {
+      findUser.userMsgs.splice(findMessage, 1);
+      res
+        .status(200)
+        .json({ user: findUser.userName, message: findUser.userMsgs });
+    }
+  }
 });
 
 app.listen(3000, () => {
