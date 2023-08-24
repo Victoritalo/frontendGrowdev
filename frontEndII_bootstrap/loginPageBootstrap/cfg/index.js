2bb0c76120c3fcce3d00aa3bc59e7e234c2ef687
victoritalo <victoritalo@live.com>
2023-08-23 22:18:48 -0300

    :books: Update aula

--- frontEndII_bootstrap/loginPageBootstrap/cfg/index.js
+++ frontEndII_bootstrap/loginPageBootstrap/cfg/index.js
@@ -2,30 +2,51 @@ const login = document.querySelector("#login");
 const alertDanger = document.querySelector(".alert-danger");
 const alertSuccess = document.querySelector(".alert-success");
 
-function loginUser() {
-  login.addEventListener("submit", async (e) => {
-    e.preventDefault();
-    const email = login.email.value;
-    const password = login.password.value;
-    try {
-      const res = await axios.post("http://127.0.0.1:3000/login", {
-        userEmail: email,
-        userPass: password,
-      });
-      console.log(res.data.message);
-      alertSuccess.innerHTML = res.data.message;
-      alertSuccess.classList.remove("displayNone");
-      setTimeout(() => {
-        alertSuccess.classList.add("displayNone");
-      }, 2500);
-    } catch (err) {
-      console.log(err);
-      alertDanger.innerHTML = err.response.data.error;
-      alertDanger.classList.remove("displayNone");
-      setTimeout(() => {
-        alertDanger.classList.add("displayNone");
-      }, 2500);
+function toggleLoadingButton(isLoading) {
+    const submitButton = document.querySelector('#submitButton');
+    const loadingSpinner = document.querySelector('#loadingSpinner');
+  
+    if (isLoading) {
+      submitButton.disabled = true;
+      loadingSpinner.style.display = 'inline-block';
+    } else {
+      submitButton.disabled = false;
+      loadingSpinner.style.display = 'none';
     }
-  });
-}
-loginUser();
+  }
+  
+  function loginUser() {
+    login.addEventListener("submit",  (e) => {
+      e.preventDefault();
+      
+      toggleLoadingButton(true);
+      const email = login.email.value;
+      const password = login.password.value;
+     setTimeout(async () => {
+        try {
+            const res = await axios.post("http://127.0.0.1:3000/login", {
+              userEmail: email,
+              userPass: password,
+            });
+      
+            console.log(res.data.message);
+            alertSuccess.innerHTML = res.data.message;
+            alertSuccess.classList.remove("displayNone");
+            setTimeout(() => {
+              alertSuccess.classList.add("displayNone");
+            }, 2500);
+          } catch (err) {
+            console.log(err);
+            alertDanger.innerHTML = err.response.data.error;
+            alertDanger.classList.remove("displayNone");
+            setTimeout(() => {
+              alertDanger.classList.add("displayNone");
+            }, 3000);
+          } finally {
+            toggleLoadingButton(false);
+          }
+     }, 2000);
+    });
+  }
+  
+  loginUser();
--- frontEndII_bootstrap/loginPageBootstrap/cfg/signup.js
+++ frontEndII_bootstrap/loginPageBootstrap/cfg/signup.js
@@ -1,37 +1,57 @@
-const signup = document.querySelector('#signup');
-const alertDanger = document.querySelector('.alert-danger');
-const alertSuccess = document.querySelector('.alert-success');
+const signup = document.querySelector("#signup");
+const alertDanger = document.querySelector(".alert-danger");
+const alertSuccess = document.querySelector(".alert-success");
 
-function signUp() { 
-    signup.addEventListener("submit",  async (e) => {
-        e.preventDefault();
-        const email = signup.email.value
-        const name = signup.name.value
-        const password = signup.password.value
-        const confirmPassword = signup.confirmPassword.value
+function toggleLoadingButton(isLoading) {
+    const submitButton = document.querySelector('#submitButton');
+    const loadingSpinner = document.querySelector('#loadingSpinner');
+  
+    if (isLoading) {
+      submitButton.disabled = true;
+      loadingSpinner.style.display = 'inline-block';
+    } else {
+      submitButton.disabled = false;
+      loadingSpinner.style.display = 'none';
+    }
+  }
+  
+  function signUp() {
+    signup.addEventListener("submit",  (e) => {
+      e.preventDefault();
+      toggleLoadingButton(true);
+  
+      const email = signup.email.value;
+      const name = signup.name.value;
+      const password = signup.password.value;
+      const confirmPassword = signup.confirmPassword.value;
+  
+     setTimeout(async () => {
         try {
             const res = await axios.post("http://127.0.0.1:3000/signup", {
-                userEmail: email,
-                userName: name,
-                userPass: password,
-                confirmPassword: confirmPassword
-            })
-            console.log(res.data.message)
-            alertSuccess.innerHTML = res.data.message
-            alertSuccess.classList.remove("displayNone")
+              userEmail: email,
+              userName: name,
+              userPass: password,
+              confirmPassword: confirmPassword
+            });
+      
+            console.log(res.data.message);
+            alertSuccess.innerHTML = res.data.message;
+            alertSuccess.classList.remove("displayNone");
             setTimeout(() => {
-                alertSuccess.classList.add("displayNone")
+              alertSuccess.classList.add("displayNone");
             }, 2500);
-            
-        } catch (err) {
-            console.log(err)
-            alertDanger.innerHTML = err.response.data.error
-            alertDanger.classList.remove("displayNone")
+          } catch (err) {
+            console.log(err);
+            alertDanger.innerHTML = err.response.data.error;
+            alertDanger.classList.remove("displayNone");
             setTimeout(() => {
-                alertDanger.classList.add("displayNone")
-            }, 2500);
-        }
-    }
-)}
-
-signUp()
+              alertDanger.classList.add("displayNone");
+            }, 3000);
+          } finally {
+            toggleLoadingButton(false);
+          }
+     }, 2000);
+    });
+  }
+  
+  signUp();
