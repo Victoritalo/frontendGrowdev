05aa25ad205c7f5b9dcc22d369fc82c1c9bdb320
victoritalo <victoritalo@live.com>
2023-09-17 22:18:43 -0300

    :books: Update Aula

--- POO_class/atividade_typescript/atividade_typescript_13_09_2023/src/Classes/employeeList.ts
+++
@@ -1,8 +0,0 @@
-// Crie um programa para mostrar informações de usuários (User) de
-// uma empresa. Crie o tipo User com as seguintes propriedades:
-// nome, idade, ocupação e salário (opcional). Caso o salário do
-// usuário não seja informado, mostre o valor “N/A”. Exemplo:
-// a. “Daphne, 23 anos, analista de TI, salário R$ 1000”
-// b. “Daphne, 23 anos, analista de TI, salário N/A”
-
-// import { IEmployee } from "../TypesProject/types"
